d23e352e4a354522fb6bee69bbf0b77f2be85bea
FlipSs <pavel@xmonetize.net>
2018-10-09 11:31:32 +0300

    update regexes

---
+++ UAParser.FormFactor.Tests/OperaMiniResourceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UAParser.FormFactor.Tests
+{
+	[TestFixture]
+	[Parallelizable]
+	public class OperaMiniResourceTests : ResourceTests
+	{
+		[Test]
+		public void CanRunOperaMiniTests()
+		{
+			RunTests("UAParser.FormFactor.Tests.Resources.opera_mini_user_agent_strings.yaml", UserAgentYamlTestCase.ReadFromMap);
+		}
+	}
+}
---
+++ UAParser.FormFactor.Tests/PodcastingResourceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UAParser.FormFactor.Tests
+{
+	[TestFixture]
+	[Parallelizable]
+	public class PodcastingResourceTests : ResourceTests
+	{
+		[Test]
+		public void CanRunPodcastingTests()
+		{
+			RunTests("UAParser.FormFactor.Tests.Resources.podcasting_user_agent_strings.yaml", UserAgentYamlTestCase.ReadFromMap);
+		}
+	}
+}
