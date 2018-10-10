7f9e55797b4aedab0aba106945fce9ae697470e3
FlipSs <pavel@xmonetize.net>
2018-10-10 08:39:07 +0300

    fix parsing windows 8.1

---
+++ UAParser.FormFactor.Tests/CustomDeviceResourceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UAParser.FormFactor.Tests
+{
+	[TestFixture]
+	[Parallelizable]
+	public class CustomDeviceResourceTests : ResourceTests
+	{
+		[Test]
+		public void CanRunCustomDeviceTests()
+		{
+			RunTests("UAParser.FormFactor.Tests.Resources.custom_test_device.yaml", OSYamlTestCase.ReadFromMap);
+		}
+	}
+}
